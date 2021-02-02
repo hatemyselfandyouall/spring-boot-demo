013867d3ebabbc4c01b61b69fd2d06fd28c18201
gaozd <gaozhengde1993@gmai.com>
2021-02-02 11:53:49 +0800

    init

--- src/main/java/com/wangxinenpu/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/wangxinenpu/springbootdemo/SpringBootDemoApplication.java
@@ -2,9 +2,11 @@ package com.wangxinenpu.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan("com.wangxinenpu.springbootdemo.dao.mapper")
 public class SpringBootDemoApplication {
 
